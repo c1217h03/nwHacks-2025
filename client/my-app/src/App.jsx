d119637883188686ec6e39b0b9b8b97d406674fc
slin04 <psymon704@gmail.com>
2025-01-18 16:58:41 -0800

    login page basic

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -4,11 +4,12 @@ import viteLogo from '/vite.svg'
 // import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
+import LoginPage from './components/LoginPage'
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <LoginPage />
     </>
 
   )
