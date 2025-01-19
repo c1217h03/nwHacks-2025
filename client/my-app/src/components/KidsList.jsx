65be858bba35017f99423af17adb5fdce95e2c07
NelsonLee2000 <nelsonleex@gmail.com>
2025-01-18 21:31:25 -0800

    fixed css

--- client/my-app/src/components/KidsList.jsx
+++ client/my-app/src/components/KidsList.jsx
@@ -20,17 +20,18 @@ export default function KidsList() {
 
     return (
         <div className="kids-list">
-            <div className="header-bar" style={{ padding: '10px 20px', display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100vw' }}>
-                <div style={{ marginLeft: 'auto', marginRight: '30px', display: 'flex' }}>
-                    <img src="/logo.jpg" alt="Logo" style={{ marginRight: '75vw', width: '50px', height: '50px' }} />
-                    <button style={{ margin: '0 10px', padding: '5px 10px' }}>Home</button>
-                    <button style={{ margin: '0 10px', padding: '5px 10px' }}>Profile</button>
-                    <button style={{ margin: '0 10px', padding: '5px 10px' }}>Logout</button>
+            <div className="header-bar">
+                <div className="navbar">
+                    <img src="/logo.jpg" alt="Logo" className="logo" />
+                    <div className="nav-buttons">
+                        <button>Home</button>
+                        <button>Profile</button>
+                        <button>Logout</button>
+                    </div>
                 </div>
-            </div >
+            </div>
 
             <div className="kids-list-body">
-
                 <div className="kids">
                     {error ? (
                         <p>{error}</p>  // Display error message if any error occurs
