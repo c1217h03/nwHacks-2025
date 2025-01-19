8ba5542213a35a5d91b1b942267850d4b53b0098
Charmaine Halim <89816977+c1217h03@users.noreply.github.com>
2025-01-18 17:29:01 -0800

    Finished Linkingn and starting on Kids List

--- client/my-app/src/App.jsx
+++ client/my-app/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
 import LoginPage from './components/LoginPage'
+import KidsList from './components/KidsList'
 
 function App() {
   return (
-    <LandingPage />
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<LandingPage />} />
+    //     <Route path="/login" element={<LoginPage />} />
+    //   </Routes>
+    // </Router>
+    <KidsList />
   );
 }
 
