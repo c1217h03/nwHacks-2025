2269fd18504b375d67bbfae678b9725069a06c32
Charmaine Halim <89816977+c1217h03@users.noreply.github.com>
2025-01-19 11:10:11 -0800

    finished the chat feature

--- client/my-app/src/components/ChatWindow.jsx
+++ client/my-app/src/components/ChatWindow.jsx
@@ -1,24 +1,49 @@
 import React from 'react';
+import { Send } from 'lucide-react';
 
 export default function ChatWindow({ post, chatVisible, toggleChatWindow }) {
     function closeChatWindow() {
         toggleChatWindow(false);
     }
+    function handleMessageChange(event) {
+        setMessage(event.target.value);
+    }
+
+    function handleSendMessage(event) {
+        event.preventDefault();
+        // Add functionality to send the message
+        console.log("Message sent:", message);
+        setMessage('');
+    }
     return (
         <div style={chatVisible ? {} : { display: "none" }} className="chat-window">
             <div className="chat-header">
-                <h3>Chat</h3>
+                <div className="chat-title">
+                    <h3>Chat</h3>
+                </div>
                 <button onClick={closeChatWindow} className="close-button">X</button>
+
             </div>
-            <div className="chat-body">
+            <div>
                 {post ? (
-                    <>
-                        <p><strong>Chatting about:</strong> {post.content}</p>
-                        {/* Add more chat functionality */}
-                    </>
+                    <div className="chat-body">
+                        <div>
+                            <p><strong>Chatting about:</strong> {post.content}</p>
+                            {/* Add more chat functionality */}
+
+                        </div>
+
+                        <div class="chat-form">
+                            <input type="text" className="message" name="message"
+                                placeholder="Enter your message here" required />
+                            <button className="chat-submit" type="submit"> <Send className='send-icon' /> </button>
+                        </div>
+                    </div>
                 ) : (
                     <p>Select a post to chat about.</p>
+
                 )}
+
             </div>
         </div>
     );
